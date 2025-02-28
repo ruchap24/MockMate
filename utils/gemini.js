c0d11a1874429f09911533324679ba69db33a9e2
ruchap24 <patilrucha991@gmail.com>
2025-02-28 12:06:23 +0530

    add google gemini api

---
+++ utils/gemini.js
@@ -0,0 +1,36 @@
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+
+// Check if API key is present
+if (!process.env.NEXT_PUBLIC_GEMINI_API_KEY) {
+  throw new Error('Missing Gemini API Key');
+}
+
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
+];
+
+export const chatSession = model.startChat({
+  generationConfig,
+  safetySettings,
+});
