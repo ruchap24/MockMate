e0d6c67e90b92f7048fdb51793f00bb8467385dd
ruchap24 <patilrucha991@gmail.com>
2025-03-06 15:10:11 +0530

    Refactor Header component for improved styling and path-based active link indication

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -8,32 +8,42 @@ import React, { useEffect } from 'react'
 function Header() {
   const path = usePathname()
   useEffect(() => {
-    console.log(path);
-  }, [])
+    console.log(path)
+  }, [path])
   
   return (
-    <div className='flex p-4 justify-around bg-secondary shadow-md items-center'>
-      <Image src={"/interviewAI.svg"} width={150} height={100} alt='Logo'></Image>
+    <div className='flex p-4 justify-around bg-[#121212] shadow-md items-center'>
+      <Image src={"/interviewAI.svg"} width={150} height={100} alt='Logo' />
       <ul className='hidden md:flex gap-5 items-center'>
         <Link href={'/dashboard'}>
-        <li className={`'hover:text-primary  cursor-pointer transition-all'
-          ${path == '/dashboard' && 'text-primary font-extrabold'}
-          `}>DashBoard</li>
+          <li
+            className={`cursor-pointer transition-all hover:text-[#7e22ce] ${
+              path === '/dashboard' ? 'text-[#7e22ce] font-extrabold' : 'text-[#ffffff]'
+            }`}
+          >
+            DashBoard
+          </li>
         </Link>
         <Link href={'/dashboard/contact'}>
-        <li className={`'hover:text-primary  cursor-pointer transition-all'
-          ${path == '/dashboard/contact' && 'text-primary font-extrabold'}
-          `}>Contact Us</li>
+          <li
+            className={`cursor-pointer transition-all hover:text-[#7e22ce] ${
+              path === '/dashboard/contact' ? 'text-[#7e22ce] font-extrabold' : 'text-[#ffffff]'
+            }`}
+          >
+            Contact Us
+          </li>
         </Link>
-
         <Link href={'/dashboard/upgrade'}>
-        <li className={`'hover:text-primary  cursor-pointer transition-all'
-          ${path == '/dashboard/upgrade' && 'text-primary font-extrabold'}
-          `}>Upgrade</li>
+          <li
+            className={`cursor-pointer transition-all hover:text-[#7e22ce] ${
+              path === '/dashboard/upgrade' ? 'text-[#7e22ce] font-extrabold' : 'text-[#ffffff]'
+            }`}
+          >
+            Upgrade
+          </li>
         </Link>
-        
       </ul>
-      <UserButton/>
+      <UserButton />
     </div>
   )
 }
