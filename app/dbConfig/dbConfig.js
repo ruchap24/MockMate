333eb56a965b0f3dabb583b0ecb90f0b62bdb2f8
ruchap24 <patilrucha991@gmail.com>
2025-03-11 14:43:01 +0530

    Remove MongoDB connection timing logs from the database configuration

--- app/dbConfig/dbConfig.js
+++ app/dbConfig/dbConfig.js
@@ -6,14 +6,7 @@ export async function connectDB() {
             throw new Error('MONGODB_URI is not defined in environment variables');
         }
 
-        console.log('Connecting to MongoDB with URI:', process.env.MONGO_URI); // Add this line
-
-        const startTime = Date.now(); // Start time
         await mongoose.connect(process.env.MONGO_URI);
-        const endTime = Date.now(); // End time
-
-        console.log(`MongoDB connection established in ${endTime - startTime}ms`); // Log time taken
-
         const connection = mongoose.connection;
 
         connection.on('connected', () => {
