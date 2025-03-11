f8e7eb7cc2a65a59979a06d8d979e5277295930c
ruchap24 <patilrucha991@gmail.com>
2025-03-11 14:15:49 +0530

    Enhance dashboard header text and add MongoDB connection timing logs for performance monitoring

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
   return (
     <div className='p-10'>
       <h1 className='font-extrabold text-primary text-3xl'>Dashboard</h1>
-      <h2 className='text-gray-500 font-semibold'>Create and Start Your AI MockUp InterView..</h2>
+      <h2 className='text-gray-500 font-semibold'>Create and Start Your AI MockUp InterView With MockMate</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-3 my-5">
         <AddNewInterview/>
